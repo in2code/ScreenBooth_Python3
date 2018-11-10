455164b4b2de762ebda7c77cce02e2a6ff23594d
Stephen <stephen@in2code.com>
2018-11-09 19:10:13 -0800

    Cleaned up comments
    Cleanup comments

--- ScreenBooth.py
+++ ScreenBooth.py
@@ -19,17 +19,11 @@ class App:
          # Gets the requested values of the height and width.
          windowWidth = self.window.winfo_screenwidth() #winfo_reqwidth()
          windowHeight = self.window.winfo_screenheight() #winfo_reqheight()
-         #print("Width",windowWidth,"Height",windowHeight)
          #for m in get_monitors():
          #     print(str(m))
  
          # open video source (by default this will try to open the computer webcam)
          self.vid = MyVideoCapture(self.video_source)
-
-         # Load an image using OpenCV
-#         self.cv_img = cv2.cvtColor(cv2.imread('recycleShawn.jpg'), cv2.COLOR_BGR2RGB)
-        # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
-        # self.height, self.width, no_channels = self.cv_img.shape
 
          # This loads an image to the background (literally)
 		 # ==========================================================
@@ -37,7 +31,6 @@ class App:
          self.cv_img = cv2.cvtColor(cv2.imread('recycleShawn.jpg'), cv2.COLOR_BGR2RGB)
 
          img = PIL.Image.fromarray(self.cv_img)
-#         img = PIL.Image.fromarray(cv2.imread('recycleShawn.jpg'))
          imgW, imgH = img.size
 
          if (imgH > imgW):
@@ -52,38 +45,19 @@ class App:
                factor = curImg / imgW
             tn_image = img.resize((int(imgW * factor), int(imgH * factor)))
 
-#         self.photo = cv2.resize(self.cv_img, dsize =(1920, 1080), interpolation = cv2.INTER_NEAREST )
-#         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.photo))
          self.photo = PIL.ImageTk.PhotoImage(image = tn_image)
-#         image = PIL.Image.open('recycleShawn.jpg')
-#         self.photo = PIL.ImageTk.PhotoImage(image)
          bk_label = tk.Label(window, image=self.photo)
          bk_label.place(x=0,y=0, relwidth=1, relheight=1) # position of background
          bk_label.image = self.photo # need this otherwise image gets destroyed and white space appears. 
-         #bk_label.pack(anchor=tk.CENTER)  #fills whole screen ...
 
          # Create a canvas that can fit the above video source size
-         #self.canvas = tkinter.Canvas(window, width = windowWidth, height = windowHeight)
          self.canvas = tk.Canvas(window, width = self.vid.width, height = self.vid.height)
-         #self.canvas = tk.Canvas(window, width = 500, height = 500) # width, height controls the box in which the video preview resides
-         #self.canvas = tkinter.Canvas(window, width = self.cv_img.shape[0], height = self.cv_img.shape[1])
          #self.canvas = tk.Entry(window)
          self.canvas.pack(pady = 50) #pady, padx moves the canvas around
-		 # load the .gif image file
-         #photo = PIL.ImageTk.PhotoImage(file='recycleShawn.jpg')
-#         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.cv_img))
 
-         # put gif image on canvas
-         # pic's upper left corner (NW) on the canvas is at x=50 y=10
-         #self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-
-         #myBtnX = windowWidth
-         #myBtnY = windowHeight
- 
          # Button that lets the user take a snapshot
          self.btn_snapshot=tk.Button(window, text="Snapshot", width=50, height = 2, command=self.snapshot)
          self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
-         #self.btn_snapshot.place(x = 100, y = 100)
  
          # Button that lets the user take a print
          self.btn_print=tk.Button(window, text="Print", width=50, height = 2, command=self.print)
